94ef8b50bd9f180ba170f084661935a8a3d74228
Bhumik47 <bhumikdehane@gmail.com>
2024-04-11 13:43:11 +0530

    httpOnly : false

--- Backend/utilities/features.js
+++ Backend/utilities/features.js
@@ -8,7 +8,7 @@ exports.sendCookie = (user, res, message, statusCode = 200) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-      httpOnly: true,
+      httpOnly: false,
       maxAge: maxAge,
       samesite: "none",
       secure: false,
